3a57497338b1ade8c4366482119f2eff8f5e98e8
D3Portillo <d3portillo@gmail.com>
2023-05-10 15:03:46 +0200

    fix(MarkdownRenderer.tsx): format for new lines and hr

--- components/SweetVault/MarkdownRenderer.tsx
+++ components/SweetVault/MarkdownRenderer.tsx
@@ -1,24 +1,39 @@
 import MarkedReact from "marked-react";
 
 function MarkdownRenderer({ content = "" }) {
+  const formattedContent = content
+    .replace("---", "\n\n---\n")
+    .replace(/ {3,}/g, "")
+
+  // Enforce `---` to be in a solo line
+  // Remove `   ` large spacing to be null (we asume it's vscode formatting)
+
   return (
     <MarkedReact
       renderer={{
+        hr() {
+          return <br />
+        },
         link(href, text) {
           return (
-            <a className="text-customPurple underline" href={href} rel="noopener noreferrer" target="_blank">
+            <a
+              className="text-customPurple underline"
+              href={href}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
               {text}
             </a>
-          );
+          )
         },
         heading(children) {
-          return <h3 className="font-medium text-lg mb-1">{children}</h3>;
+          return <h3 className="font-medium text-lg mb-1">{children}</h3>
         },
       }}
       breaks
       gfm
     >
-      {content}
+      {formattedContent}
     </MarkedReact>
   );
 }
