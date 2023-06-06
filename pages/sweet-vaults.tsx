201021c6f0a0dadfd792f6821aa985c761048945
RedVeil <leon.niesler@gmail.com>
2023-06-06 14:29:28 +0200

    hide new vault

--- pages/sweet-vaults.tsx
+++ pages/sweet-vaults.tsx
@@ -3,15 +3,18 @@ import SweetVaults, { SUPPORTED_NETWORKS } from "components/SweetVault/SweetVaul
 import useNetworkFilter from "hooks/useNetworkFilter";
 import { useAllVaults } from "hooks/vaults";
 import { ChainId } from "lib/utils";
+import { Contract, ethers } from "ethers";
 
 
 const HIDDEN_VAULTS = ["0xb6cED1C0e5d26B815c3881038B88C829f39CE949", "0x2fD2C18f79F93eF299B20B681Ab2a61f5F28A6fF",
-"0xF1649eC625Aca15831237D29cd09F3c71a5cca63", // ystEth factory
-"0xcBb5A4a829bC086d062e4af8Eba69138aa61d567", // yOhmFrax factory
-"0x759281a408A48bfe2029D259c23D7E848A7EA1bC", // stYCrv
-"0x9E237F8A3319b47934468e0b74F0D5219a967aB8", // yABoosted Balancer
-"0xDFf04Efb38465369fd1A2E8B40C364c22FfEA340", // y3Crypto
+ "0xDFf04Efb38465369fd1A2E8B40C364c22FfEA340","0xd4D442AC311d918272911691021E6073F620eb07", //@dev for some reason the live 3Crypto yVault isnt picked up by the yearnAdapter nor the yearnFactoryAdapter
+  "0xF1649eC625Aca15831237D29cd09F3c71a5cca63", // ystEth factory
+  "0xcBb5A4a829bC086d062e4af8Eba69138aa61d567", // yOhmFrax factory
+  "0x759281a408A48bfe2029D259c23D7E848A7EA1bC", // stYCrv
+  "0x9E237F8A3319b47934468e0b74F0D5219a967aB8", // yABoosted Balancer
 ]
+
+
 
 const PopSweetVaults: NextPage = () => {
   const [selectedNetworks, selectNetwork] = useNetworkFilter(SUPPORTED_NETWORKS);
