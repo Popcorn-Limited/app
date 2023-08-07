d843840912e98dcf9d64eaea5c59762c61fee869
RedVeil <leon.niesler@gmail.com>
2023-08-07 17:25:18 +0200

    somewhat addresses css issues

--- components/SecondaryActionButton.tsx
+++ components/SecondaryActionButton.tsx
@@ -16,7 +16,7 @@ const SecondaryActionButton: React.FC<SecondaryActionButtonProps> = ({
   return (
     <button
       type="button"
-      className={`${className} whitespace-nowrap px-8 py-3 font-medium text-base transition-all ease-in-out duration-500 w-full flex flex-row items-center justify-center bg-white border border-primary text-primary rounded-4xl hover:bg-primary hover:text-white disabled:bg-white disabled:border-secondaryLight disabled:text-secondaryLight disabled:hover:border-secondaryLight disabled:hover:text-secondaryLight disabled:hover:bg-white ${
+      className={`${className} px-8 py-3 font-medium text-base transition-all ease-in-out duration-500 w-full flex flex-row items-center justify-center bg-white border border-primary text-primary rounded-4xl hover:bg-primary hover:text-white disabled:bg-white disabled:border-secondaryLight disabled:text-secondaryLight disabled:hover:border-secondaryLight disabled:hover:text-secondaryLight disabled:hover:bg-white ${
         hidden ? "hidden" : ""
       }`}
       onClick={handleClick}
