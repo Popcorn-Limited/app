9ad502f5f30c145f0d2f8d0772b02cefa21edeee
RedVeil <leon.niesler@gmail.com>
2023-08-29 18:44:35 +0200

    approval fix

--- hooks/useApproveBalance.ts
+++ hooks/useApproveBalance.ts
@@ -13,7 +13,7 @@ const useApproveBalance = (
     address: assetAddress,
     abi: ["function approve(address spender, uint256 amount) public"],
     functionName: "approve",
-    args: [spender, constants.MaxUint256],
+    args: [spender, 115792089237316195423570985008687907853269984665640], // approve maxUint256 / 1e27 cause metaMask rounds stuff weirdly when you are above 18 decimals
     chainId: Number(chainId),
   });
 
