8f0f4c8d0f32cece0f11be5a334a0c42876977ef
RedVeil <leon.niesler@gmail.com>
2023-09-14 10:49:15 +0200

    canVote

--- lib/Gauges/useHasAlreadyVoted.ts
+++ lib/Gauges/useHasAlreadyVoted.ts
@@ -1,27 +1,31 @@
 import { BigNumber } from 'ethers';
-import { useContractReads } from 'wagmi';
 import { Pop } from 'lib/types';
+import { getVeAddresses } from 'lib/utils/addresses';
+import { Address, useContractReads } from 'wagmi';
 
 const DAYS = 24 * 60 * 60;
 
-export function useLastUserVotes({ addresses, chainId, account }: { addresses: string[], chainId: number, account: string }): { data: boolean, status: string } {
-    console.log("DING");
+const { GaugeController: GAUGE_CONTROLLER } = getVeAddresses()
+
+export function useHasAlreadyVoted({ addresses, chainId, account }: { addresses: string[], chainId: number, account: string }): { data: boolean, status: string } {
     const { data, status } = useContractReads({
-        contracts: addresses.map((address) => {
+        contracts: addresses?.map((address) => {
             return {
-                address: address,
+                address: GAUGE_CONTROLLER as Address,
                 abi: abiController,
                 functionName: "last_user_vote",
                 chainId: chainId,
                 args: [account, address]
             }
         }),
+        enabled: !!account && addresses?.length > 0,
     }) as Pop.HookResult<BigNumber[]>
 
     const limitTimestamp = Math.floor(Date.now() / 1000) - (DAYS * 10);
+    console.log(data)
 
-    const alreadyVoted = data.some(voteTimestamp =>
-        voteTimestamp.gt(BigNumber.from(limitTimestamp))
+    const alreadyVoted = data?.some(voteTimestamp =>
+        voteTimestamp?.gt(BigNumber.from(limitTimestamp))
     );
 
     return { data: alreadyVoted, status: status };
