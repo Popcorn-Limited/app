dcd1e0371f0fdc8cae6bc5376e2aa006c5e74a0c
RedVeil <leon.niesler@gmail.com>
2023-10-24 17:39:27 +0200

    added custom time

--- components/vepop/modals/manage/IncreaseTimeInteface.tsx
+++ components/vepop/modals/manage/IncreaseTimeInteface.tsx
@@ -30,18 +30,22 @@ export default function IncreaseTimeInterface({ daysState, lockedBal }:
       <h2 className="text-start text-5xl">Increase lock time</h2>
 
       <div>
-        <p className="text-primary font-semibold mb-1">Lockup Time</p>
-        <div className="flex flex-row items-center space-x-2">
+        <div className="flex flex-row items-center justify-between">
+          <p className="text-primary font-semibold mb-1">Lockup Time</p>
+          <p className="w-32 text-secondaryLight">Custom Time</p>
+        </div>
+        <div className="flex flex-row items-center justify-between">
           <LockTimeButton label="1W" isActive={days === 7} handleClick={() => setDays(7)} />
           <LockTimeButton label="1M" isActive={days === 30} handleClick={() => setDays(30)} />
           <LockTimeButton label="3M" isActive={days === 90} handleClick={() => setDays(90)} />
           <LockTimeButton label="6M" isActive={days === 180} handleClick={() => setDays(180)} />
           <LockTimeButton label="1Y" isActive={days === 365} handleClick={() => setDays(365)} />
           <LockTimeButton label="2Y" isActive={days === 730} handleClick={() => setDays(730)} />
-          <div className="w-40 flex px-5 py-2 items-center rounded-lg border border-customLightGray">
+          <LockTimeButton label="4Y" isActive={days === 1460} handleClick={() => setDays(1460)} />
+          <div className="w-32 flex px-5 py-2 items-center rounded-lg border border-customLightGray">
             <InputNumber
               onChange={handleSetDays}
-              defaultValue={days}
+              value={days}
               autoComplete="off"
               autoCorrect="off"
               type="text"
