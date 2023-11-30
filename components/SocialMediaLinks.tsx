46ae00641c75785d16024ff83ce46c94ac1696e4
RedVeil <leon.niesler@gmail.com>
2023-11-30 11:16:04 +0100

    updated social-links

--- components/SocialMediaLinks.tsx
+++ components/SocialMediaLinks.tsx
@@ -1,28 +1,24 @@
 import DiscordIcon from "@/components/svg/socialMedia/DiscordIcon";
 import MediumIcon from "@/components/svg/socialMedia/MediumIcon";
 import RedditIcon from "@/components/svg/socialMedia/RedditIcon";
-import TelegramIcon from "@/components/svg/socialMedia/TelegramIcon";
 import TwitterIcon from "@/components/svg/socialMedia/TwitterIcon";
 import YoutubeIcon from "@/components/svg/socialMedia/YoutubeIcon";
 
 export default function SocialMediaLinks() {
   return <>
-    <a href="https://twitter.com/Popcorn_DAO">
+    <a href="https://twitter.com/VaultCraft_io" target="_blank">
       <TwitterIcon color={"#645F4B"} size={"24"} />
     </a>
-    <a href="https://discord.gg/NYgNA6wv">
+    <a href="https://discord.gg/ujxSKu2H" target="_blank">
       <DiscordIcon color={"#645F4B"} size={"24"} />
     </a>
-    <a href="https://t.me/popcorndaochat">
-      <TelegramIcon color={"#645F4B"} size={"24"} />
-    </a>
-    <a href="https://medium.com/popcorndao">
+    <a href="https://medium.com/popcorndao" target="_blank">
       <MediumIcon color={"#645F4B"} size={"24"} />
     </a>
-    <a href="https://www.reddit.com/r/popcorndao/">
+    <a href="https://www.reddit.com/r/popcorndao/" target="_blank">
       <RedditIcon color={"#645F4B"} size={"24"} />
     </a>
-    <a href="https://www.youtube.com/channel/UCe8n8mGG4JR7nhFT4v9APyA">
+    <a href="https://www.youtube.com/channel/UCe8n8mGG4JR7nhFT4v9APyA" target="_blank">
       <YoutubeIcon color={"#645F4B"} size={"24"} />
     </a>
   </>
